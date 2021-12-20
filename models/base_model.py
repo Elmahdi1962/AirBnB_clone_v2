052badec8bb02dcc2a1acaf69d2d32de61dc53cf
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-12-20 19:17:27 +0100

    fixed task 2 bug

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
 from models import storage_type
 
-Base = declarative_base()
+Base = declarative_base() if storage_type == 'db' else object
 
 
 class BaseModel:
