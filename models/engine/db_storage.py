e01b06b7fcc48ca7b3175b71ccc2f7c10c8dc286
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-12-20 21:48:13 +0100

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,7 +7,7 @@ from os import getenv
 from models.amenity import Amenity
 from models.base_model import Base
 from models.city import City
-from models.place import Place
+from models.place import Place, place_amenity
 from models.review import Review
 from models.state import State
 from models.user import User
