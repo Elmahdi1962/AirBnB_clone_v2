317ea3160424e03e6e8d22368a621164b61f7c98
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-12-21 18:59:35 +0100

    update tests

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -9,20 +9,20 @@ class test_City(test_basemodel):
     """ """
 
     def __init__(self, *args, **kwargs):
-        """init of city test unit """
+        """ """
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
     def test_state_id(self):
-        """ testing state id"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.state_id), str if
                          os.getenv('HBNB_TYPE_STORAGE') != 'db' else
                          type(None))
 
     def test_name(self):
-        """ testinf city name"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.name), str if
                          os.getenv('HBNB_TYPE_STORAGE') != 'db' else
