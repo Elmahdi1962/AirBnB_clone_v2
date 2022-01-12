0e54e94ddca89c9a597123033c4afa80bb42883f
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-01-12 20:34:25 +0100

    fixed path

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -56,13 +56,13 @@ def do_deploy(archive_path):
     if res.failed:
         return False
 
-    res = run('mv data/web_static/releases/{}/web_static/* ' +
-              '/data/web_static/releases/{}/'
+    res = run(('mv /data/web_static/releases/{}/web_static/* ' +
+              '/data/web_static/releases/{}/')
               .format(file_name, file_name))
     if res.failed:
         return False
 
-    res = run('rm -rf data/web_static/releases/{}/web_static'
+    res = run('rm -rf /data/web_static/releases/{}/web_static'
               .format(file_name))
     if res.failed:
         return False
