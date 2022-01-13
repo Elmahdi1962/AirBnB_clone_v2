6228e82065ccf495c9335f6edb580b8e55ace466
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-01-13 11:14:32 +0100

    update data path

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,7 @@ sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo "<h1>Test Page</h1>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data
+sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i "/server_name _;/a \\\n\tlocation /hbnb_static { \n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
 sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
 sudo service nginx start
