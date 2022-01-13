c8103bf398a295c0219db08e9725baf2c19637a1
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-01-13 16:25:34 +0100

    undo last commit + update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,79 @@
 #!/usr/bin/python3
-"""
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
-"""
+'''fabric script for task 2 0X03'''
 
-from fabric.api import put, run, env
-from os.path import exists
-env.hosts = ['142.44.167.228', '144.217.246.195']
+from fabric.api import local, put, run, env
+from fabric.decorators import runs_once
+from datetime import datetime
+import re
+from os import path
+
+
+env.hosts = [
+    '34.75.211.145',
+    '3.236.217.0'
+]
+
+
+@runs_once
+def do_pack():
+    '''generates a .tgz archive from the contents of the web_static folder'''
+    local("mkdir -p versions")
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
+
+    if result.failed:
+        return None
+    return result
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    '''distributes an archive to my web servers'''
+    if not path.exists(archive_path):
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        file_name = re.search(r'versions/(\S+).tgz', archive_path)
+        if file_name is None:
+            return False
+        file_name = file_name.group(1)
+        res = put(local_path=archive_path, remote_path="/tmp/{}.tgz"
+                  .format(file_name))
+        if res.failed:
+            return False
+
+        res = run("mkdir -p /data/web_static/releases/{}".format(file_name))
+        if res.failed:
+            return False
+
+        res = run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+                  .format(file_name, file_name))
+        if res.failed:
+            return False
+
+        res = run('rm -rf /tmp/{}.tgz'.format(file_name))
+        if res.failed:
+            return False
+
+        res = run(('mv /data/web_static/releases/{}/web_static/* ' +
+                  '/data/web_static/releases/{}/')
+                  .format(file_name, file_name))
+        if res.failed:
+            return False
+
+        res = run('rm -rf /data/web_static/releases/{}/web_static'
+                  .format(file_name))
+        if res.failed:
+            return False
+
+        res = run('rm -rf /data/web_static/current')
+        if res.failed:
+            return False
+
+        res = run(('ln -s /data/web_static/releases/{}/' +
+                  ' /data/web_static/current')
+                  .format(file_name))
+        if res.failed:
+            return False
         return True
     except Exception:
         return False
