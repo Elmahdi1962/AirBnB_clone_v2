0a549aedb1bd433af32aba9482bfd5b11528bf17
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-01-13 11:08:33 +0100

    removed if statement

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,11 +5,7 @@ sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo "<h1>Test Page</h1>" > /data/web_static/releases/test/index.html
-if [ -d "/data/web_static/current" ];
-then
-    echo "path /data/web_static/current exists"
-    rm -rf /data/web_static/current;
-fi;
+
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data
 sed -i "/server_name _;/a \\\n\tlocation /hbnb_static { \n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
