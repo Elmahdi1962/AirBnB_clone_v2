7ee820e46cf932b1f3d8cec0186232647563edf3
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-01-13 11:11:04 +0100

    add sudo to sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,9 +5,8 @@ sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo "<h1>Test Page</h1>" > /data/web_static/releases/test/index.html
-
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data
-sed -i "/server_name _;/a \\\n\tlocation /hbnb_static { \n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+sudo sed -i "/server_name _;/a \\\n\tlocation /hbnb_static { \n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
 sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
 sudo service nginx restart
