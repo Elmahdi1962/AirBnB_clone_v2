3f7f40116c8d54e43817e74f953399723386d8e3
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-01-13 10:37:03 +0100

    added / to /data

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt-get -y install nginx
 ufw allow 'Nginx HTTP'
 mkdir -p /var/www/html
 sudo chmod -R 755 /var/www
-echo 'Hello World!' > /var/www/html/index.html
+echo 'Hello World!' > /var/www/html/index.html 
 echo -e "Ceci n\x27est pas une page" > /var/www/html/404.html
 mkdir -p /data/web_static/
 mkdir -p /data/web_static/shared/
@@ -17,7 +17,7 @@ then
     rm -rf /data/web_static/current;
 fi;
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data
+sudo chown -hR ubuntu:ubuntu /data/
 sed -i "/server_name _;/a \\\n\tlocation /hbnb_static { \n\t\talias /data/web_static/current/;\n\t\tautoindex on;\n\t}" /etc/nginx/sites-available/default
 ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
 sudo service nginx restart
