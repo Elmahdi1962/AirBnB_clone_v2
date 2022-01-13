c2841a03cb3db40861f9538ff811bc2170ed04da
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-01-13 10:52:58 +0100

    undo last commit and added upgrade command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 # sets up my web servers for the deployment of web_static
-sudo apt-get -y update
+sudo apt -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+ufw allow 'Nginx HTTP'
+mkdir -p /var/www/html
+sudo chmod -R 755 /var/www
+echo 'Hello World!' > /var/www/html/index.html 
+echo -e "Ceci n\x27est pas une page" > /var/www/html/404.html
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "<h1>Test Page</h1>" > /data/web_static/releases/test/index.html
+if [ -d "/data/web_static/current" ];
+then
+    echo "path /data/web_static/current exists"
+    rm -rf /data/web_static/current;
+fi;
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+sudo chown -hR ubuntu:ubuntu /data
+sed -i "/server_name _;/a \\\n\tlocation /hbnb_static { \n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+sudo service nginx restart
