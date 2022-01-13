f64e82f5ee0531c439bc2b92233f1736f9576ba2
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-01-13 11:03:03 +0100

    undo last commit and comment other line

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,7 +2,7 @@
 # sets up my web servers for the deployment of web_static
 sudo apt -y update
 sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+# sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo "<h1>Test Page</h1>" > /data/web_static/releases/test/index.html
 if [ -d "/data/web_static/current" ];
@@ -13,5 +13,5 @@ fi;
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data
 sed -i "/server_name _;/a \\\n\tlocation /hbnb_static { \n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
-# sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
 sudo service nginx restart
