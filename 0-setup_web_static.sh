2062c5f10214ec23026b25d13a6dc777746698e6
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-01-13 11:16:13 +0100

    update paths

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,7 @@
 sudo apt -y update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
-sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "<h1>Test Page</h1>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
