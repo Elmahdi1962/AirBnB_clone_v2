fce69a8c4e91198805247f7e26749e15b099aa68
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-01-13 12:03:20 +0100

    undo last commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,37 +12,7 @@ then
 fi;
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data
-config=\
-"
-server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        root /var/www/html;
-        # Add index.php to the list if you are using PHP
-        index index.html index.htm index.nginx-debian.html;
-        add_header X-Served-By \$hostname;
-        server_name _;
-
-        location / {
-                try_files \$uri \$uri/ =404;
-        }
-
-        location /hbnb_static/ { 
-            alias /data/web_static/current/;
-        }
-
-        error_page 404 /404.html;
-        location  /404.html {
-            internal;
-	    }
-
-        }
-        
-        if (\$request_filename ~ redirect_me){
-            rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-        }
-}
-"
-echo "$config" > /etc/nginx/sites-available/default
+#sed -i "/server_name _;/a \\\n\tlocation /hbnb_static { \n\t\talias /data/web_static/current/;\n\t\tautoindex on;\n\t}" /etc/nginx/sites-available/default
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
 sudo service nginx restart
